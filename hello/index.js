eae3aee9157e5409723100547fde49fc698dc2ae
WillHart2002 <79221738+WillHart2002@users.noreply.github.com>
2024-07-11 14:24:04 -0400

    first biggg commit

---
+++ hello/index.js
@@ -0,0 +1,33 @@
+// fnm env --use-on-cd | Out-String | Invoke-Expression
+// fnm use --install-if-missing 20
+// node -v
+// npm -v
+// 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
+
+const http = require('http')
+const fs = require('fs')
+
+const PORT = 3000;
+
+const server = http.createServer(function(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    fs.readFile('index.html', function(error, data) {
+        if (error) {
+            res.writeHead(404)
+            res.write('Error: file not found')
+        } else {
+            res.write(data)
+        }
+        res.end()
+    })
+})
+
+server.listen(PORT, function(error){
+    if (error) {
+        console.log("Something went wrong")
+    } else {
+        console.log("Server is listening on port " + PORT)
+    }
+})
+
+console.log("Hello world")
